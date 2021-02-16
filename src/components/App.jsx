a1d2bec9f557cdcaabd800b8376b15f124fffeb4
Samy Amraoui <sametzy@gmail.com>
2021-02-16 21:47:24 +0100

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+export class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Firememe</h1>
+            </div>
+        );
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        index: './src/index.js'
+    },
+    output: {
+        path: __dirname + '/build',
+        filename: 'index.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js|\.jsx$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+};
