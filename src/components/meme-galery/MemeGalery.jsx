b9be0f74b48a187060378bf19aeaf98a956c7a56
Samy Amraoui <sametzy@gmail.com>
2021-02-21 12:39:08 +0000

    Move data logic to MemeGalery parent component

--- src/components/meme-galery/MemeGalery.jsx
+++ src/components/meme-galery/MemeGalery.jsx
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 import { MemeList } from './meme-list';
+import { getMemeCollection } from '../../services/store';
 
 export class MemeGalery extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            memes: []
+        };
+    }
+    componentDidMount() {
+        getMemeCollection().onSnapshot(snapshot => {
+            const memes = snapshot.docs.map(doc => {
+                const meme = doc.data();
+                return {
+                    id: doc.id,
+                    author: meme.author,
+                    date: meme.date.toString(),
+                    file: meme.file
+                };
+            });
+            this.setState({ memes });
+        });
+    }
+
     render() {
         return (
             <div>
-                <MemeList />
+                <MemeList memes={this.state.memes} />
             </div>
         );
     }
--- src/components/meme-galery/meme-list/MemeList.jsx
+++ src/components/meme-galery/meme-list/MemeList.jsx
@@ -1,35 +1,12 @@
 import React, { Component } from 'react';
-import { getMemeCollection } from '../../../services/store';
 import { MemeDoc } from '../meme-doc';
 import './MemeList.css';
 
 export class MemeList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            memes: []
-        };
-    }
-
-    componentDidMount() {
-        getMemeCollection().onSnapshot(snapshot => {
-            const memes = snapshot.docs.map(doc => {
-                const meme = doc.data();
-                return {
-                    id: doc.id,
-                    author: meme.author,
-                    date: meme.date.toString(),
-                    file: meme.file
-                };
-            });
-            this.setState({ memes });
-        });
-    }
-
     render() {
         return (
             <div className="list">
-                {this.state.memes.map(meme => (
+                {this.props.memes.map(meme => (
                     <div key={meme.id}
                          className="element">
                         <MemeDoc meme={meme} />
