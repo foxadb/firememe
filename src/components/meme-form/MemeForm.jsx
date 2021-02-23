a350ba4ccfdc27ab5696c9546fc084150cbc4a93
Samy Amraoui <sametzy@gmail.com>
2021-02-23 21:11:22 +0000

    Add image validation in MemeForm component

--- src/components/meme-form/MemeForm.jsx
+++ src/components/meme-form/MemeForm.jsx
@@ -8,11 +8,12 @@ export class MemeForm extends Component {
         super(props);
         this.state = {
             author: '',
-            file: ''
+            file: '',
+            error: ''
         };
         this.changeAuthor = this.changeAuthor.bind(this);
         this.changeFile = this.changeFile.bind(this);
-        this.sendMeme = this.sendMeme.bind(this);
+        this.submitMeme = this.submitMeme.bind(this);
     }
 
     changeAuthor(event) {
@@ -23,25 +24,40 @@ export class MemeForm extends Component {
         this.setState({ file: event.target.files[0] });
     }
 
-    sendMeme(event) {
-        fileToBase64(this.state.file).then(base64File => {
-            getMemeCollection().add({
-                author: this.state.author,
-                date: new Date(),
-                file: base64File
-            }).then(doc => {
-                console.log(`Meme ${doc.id} created`);
-            }).catch(error => {
-                console.error('Error when creating meme', error);
+    submitMeme(event) {
+        const { author, file } = this.state;
+        const isBigFile = file.size > 2097152;
+        const isImageFile = /^image\/(jpeg|png)$/.test(file.type);
+        if (isBigFile) {
+            this.setState({ error: 'Maximum image size is 2 MB' });
+        } else if (!isImageFile) {
+            this.setState({ error: 'Only jpeg and png formats are supported' });
+        } else {
+            this.setState({ error: null });
+            fileToBase64(file).then(base64File => {
+                getMemeCollection().add({
+                    author,
+                    date: new Date(),
+                    file: base64File
+                }).then(doc => {
+                    console.log(`Meme ${doc.id} created`);
+                }).catch(error => {
+                    console.error('Error when creating meme', error);
+                });
             });
-        });
-
+        }
         event.preventDefault();
     }
 
     render() {
+        const errorMessage = this.state.error &&
+            <div className="form-group">
+                <span className="error">
+                    {this.state.error}
+                </span>
+            </div>;
         return (
-            <form onSubmit={this.sendMeme}>
+            <form onSubmit={this.submitMeme}>
                 <div className="form-group">
                     <label>Author</label>
                     <div>
@@ -61,6 +77,7 @@ export class MemeForm extends Component {
                     <input type="submit"
                            value="Submit" />
                 </div>
+                {errorMessage}
             </form>
         );
     }
